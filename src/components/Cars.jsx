0e63643a6c0ba6a8c2f7ef003e54ba5d6db12e88
UrosVesic <uros99uki@gmail.com>
2022-02-02 20:47:09 +0100

    cars komponenta

---
+++ src/components/Cars.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Car from "./Car.jsx";
+
+const Cars = () => {
+  return (
+    <div className="all-cars">
+      <Car />
+      <Car />
+      <Car />
+    </div>
+  );
+
+};
+
+export default Cars;
